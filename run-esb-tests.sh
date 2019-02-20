3675431e0e6b79ff26ff67679e7810a59feb10a4
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2019-02-20 17:35:05 +0530

    update proxy-test.jmx and run-esb-tests.sh

--- run-esb-tests.sh
+++ run-esb-tests.sh
@@ -1,12 +1,22 @@
 declare -a arr=("500" "1024" "5120" "10240" "102400" "512000")
+declare -a no_of_users=("100", "200", "500", "1000")
 
 # clear existing results
-cd results/esb
-find . -type f -iname \*.csv -delete
+# cd results/esb
+# find . -type f -iname \*.csv -delete
 
 
 for i in "${arr[@]}"
 do
-    jmeter -n -t proxy-test.jmx -Jsize=$i -Jproxy=esb -JserverName='Menukas-MacBook-Pro.local' -Jport=8280 -Jpath='/services/EchoEndpoint'
+    for j in "${no_of_users[@]}"
+        do
+            jmeter -n -t proxy-test.jmx 
+                -Jsize=$i \
+                -Jproxy=esb \
+                -JserverName='Menukas-MacBook-Pro.local' \
+                -Jport=8280 \
+                -Jpath='/services/EchoEndpoint' \
+                -l results/esb-payload$i-users$j.jtl
+        done
 done
 
