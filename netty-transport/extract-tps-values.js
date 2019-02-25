8a06f0f60dbc53d68cc4723fcd41144cf225331d
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2019-02-26 01:26:28 +0530

    create csvs

---
+++ netty-transport/extract-tps-values.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const {convertArrayToCSV} = require('convert-array-to-csv');
+
+const proxy = "without-netty";
+const resultsDir = "results/" + proxy;
+
+const payloads = ["500", "1024", "5120", "10240", "102400", "512000"];
+const users = ["100", "200", "500", "1000"];
+
+let records = [];
+
+payloads.forEach((payload) => {
+    console.log("Payload:", payload);
+    console.log('================');
+    users.forEach((users) => {
+        let summaryFileCommon = proxy + "-payload" + payload + "-users" + users;
+        let summaryFilePath = resultsDir + "/" + summaryFileCommon + "/" + summaryFileCommon + "-measurement-summary.json";
+        if (fs.existsSync(summaryFilePath)) {
+            console.log(summaryFileCommon);
+            let summaryJson = JSON.parse(fs.readFileSync(summaryFilePath, 'utf-8'));
+
+            let jsonHttpRequest = summaryJson["HTTP Request"];
+            console.log(jsonHttpRequest);
+            if (jsonHttpRequest) {
+                let throughput = jsonHttpRequest.throughput;
+                let errorPercentage = jsonHttpRequest.errorPercentage;
+
+                console.log("Throughput:", throughput);
+                console.log("ErrorPercentage:", errorPercentage);
+
+                records.push({
+                    payload: payload,
+                    users: users,
+                    errorPercentage: errorPercentage,
+                    throughput: throughput
+                });
+            }
+        }
+        console.log();
+    });
+    console.log();
+});
+
+const csvs = convertArrayToCSV(records);
+console.log(csvs);
+
+
+fs.writeFile('csvs/' + proxy + '.csv', csvs, function (err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('...Done');
+});
